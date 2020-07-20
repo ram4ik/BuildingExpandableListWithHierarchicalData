4ae2e69e6d11ec90f96b1f655e1142bc970d75d1
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-20 12:42:19 +0300

    Initial Commit

---
+++ BuildingExpandableListWithHierarchicalData/BuildingExpandableListWithHierarchicalDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingExpandableListWithHierarchicalDataApp.swift
+//  BuildingExpandableListWithHierarchicalData
+//
+//  Created by ramil on 20.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct BuildingExpandableListWithHierarchicalDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
